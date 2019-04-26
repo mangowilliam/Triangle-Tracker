e7b8c6ebda50117b8b7c970690889c70a88ba94d
mangowilliam <williammango2015@gmail.com>
2019-04-26 19:24:21 +0300

    add isosceles conditions on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-function getSide() {
+function getShape() {
   var base = parseFloat(prompt("enter the value of the side facing downwards"));
   var leftLeg = parseFloat(prompt("enter the value of the left side "));
   var rightLeg = parseFloat(prompt("enter the value of the right side"));
   if (base === leftLeg && leftLeg === rightLeg && basse === rightLeg) {
-    alert("Equilateral-Trianle!!")
+    alert("Equilateral Trianle!!");
+  } else if (base === leftLeg || base === rightLeg || leftLeg === rightLeg) {
+    alert("Isosceles Triangle!!");
+  } else if (base + leftLeg) {
+
   }
 }
