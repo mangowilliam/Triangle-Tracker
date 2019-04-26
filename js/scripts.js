a92c365a7dbcec6ed521e538c4281d9ead753e85
mangowilliam <williammango2015@gmail.com>
2019-04-26 19:07:55 +0300

    add if control flow and equilateral conditions

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function getSide() {
+  var base = parseFloat(prompt("enter the value of the side facing downwards"));
+  var leftLeg = parseFloat(prompt("enter the value of the left side "));
+  var rightLeg = parseFloat(prompt("enter the value of the right side"));
+  if (base === leftLeg && leftLeg === rightLeg && basse === rightLeg) {
+    alert("Equilateral-Trianle!!")
+  }
+}
