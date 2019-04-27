3d6ec48d6ab691ee2d9b04087c590a45a2767743
mangowilliam <williammango2015@gmail.com>
2019-04-27 09:55:22 +0300

    amend control flow condition for equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function getShape() {
   var base = parseFloat(prompt("enter the value of the side facing downwards"));
   var leftLeg = parseFloat(prompt("enter the value of the left side "));
   var rightLeg = parseFloat(prompt("enter the value of the right side"));
-  if (base === leftLeg && leftLeg === rightLeg && basse === rightLeg) {
+  if (base === leftLeg && leftLeg === rightLeg) {
     alert("Equilateral Trianle!!");
   } else if (base === leftLeg || base === rightLeg || leftLeg === rightLeg) {
     alert("Isosceles Triangle!!");
