0c9ff1a7138a84e3f3c1f20188a015aa44ac58e4
mangowilliam <williammango2015@gmail.com>
2019-04-27 17:02:01 +0300

    modify js file to evaluate num only

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 function getShape() {
-  var base = parseFloat(prompt("enter the value of the side facing downwards"));
-  var leftLeg = parseFloat(prompt("enter the value of the left side "));
-  var rightLeg = parseFloat(prompt("enter the value of the right side"));
-  if (base === leftLeg && leftLeg === rightLeg) {
-    alert("Equilateral Trianle!!");
+  var base = parseInt(prompt("enter the value of the side facing downwards"));
+  var leftLeg = parseInt(prompt("enter the value of the left side "));
+  var rightLeg = parseInt(prompt("enter the value of the right side"));
+  if (isNaN(base) || isNaN(leftLeg) || isNaN(rightLeg)) {
+    alert("error");
+  } else if (base === leftLeg && leftLeg === rightLeg) {
+    alert("Equilateral Triangele!!");
   } else if (base === leftLeg || base === rightLeg || leftLeg === rightLeg) {
     alert("Isosceles Triangle!!");
   } else if ((base + leftLeg) <= rightLeg || (base + rightLeg) <= leftLeg || (leftLeg + rightLeg) <= base) {
     alert("Not a Triangle!!");
-  } else {
+  } else if ((base != leftLeg && base != rightLeg)) {
     alert("scalene Triange!!");
+  } else {
+    alert("");
   }
 }
