dbf2211e227cac55bbcb41ad7462a7eb67f1b7cc
mangowilliam <williammango2015@gmail.com>
2019-04-27 20:20:29 +0300

    add a form

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,37 @@
 function getShape() {
-  var side = []
+  var side = [] //create an array
   side.push(document.getElementById("base").value);
   side.push(document.getElementById("leftLeg").value);
   side.push(document.getElementById("rightLeg").value);
-
-  var base = side[0]
-  var leftLeg = side[1]
-  var rightLeg = side[2]
-
-  if (isNaN(base) || isNaN(leftLeg) || isNaN(rightLeg)) {
-    alert("error");
-  } else if (base === leftLeg && leftLeg === rightLeg) {
-    alert("Equilateral Triangele!!");
-  } else if (base === leftLeg || base === rightLeg || leftLeg === rightLeg) {
-    alert("Isosceles Triangle!!");
-  } else if ((base + leftLeg) <= rightLeg || (base + rightLeg) <= leftLeg || (leftLeg + rightLeg) <= base) {
-    alert("Not a Triangle!!");
-  } else if ((base != leftLeg && base != rightLeg)) {
-    alert("scalene Triange!!");
+  //push side into array
+  var [b, l, r] = side //assign
+  var output = document.getElementById(output);
+  if (b.length === 0 || l.length === 0 || r.length === 0) {
+    output.innerHTML = ("insert your values")
   } else {
-    alert("");
+    b = parseInt(b);
+    l = parseInt(l);
+    r = parseInt(r);
+    findIf(b, l, r);
   }
 }
-$(document).ready(getShape() {
-      $("form").submit(getShape(vent) {
+
+function findIf(b, l, r) {
+  if (b === l && l === r) {
+    output.innerHTML = ("Equilateral Triangele!!")
+  } else if (b === l || base === r || l === r) {
+    output.innerHTML = ("Isosceles Triangle!!")
+  } else if ((b + l) <= r || (b + r) <= l || (l + r) <= b) {
+    output.innerHTML = ("Not a Triangle!!")
+  } else if (b != l && b != r) {
+    output.innerHTML = ("scalene Triange!!")
+  } else if (isNan(b) || isNan(l) || isNan(r)) {
+    output.innerHTML = ("not a number")
+  } else {
+    output.innerHTML = //("do nothing")
+  }
+}
+
+function reset() {
+  location.reload();
+}
