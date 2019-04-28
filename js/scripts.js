6492f97e7083fb7e488e289b7c36ef5569a252ef
mangowilliam <williammango2015@gmail.com>
2019-04-28 23:13:08 +0300

    add updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
-function tringle() {
-  var edges = [] //create an array
-  edges.push(document.getElementById("base").value);
-  edges.push(document.getElementById("leftLeg").value);
-  edges.push(document.getElementById("rightLeg").value);
-  //push side into array
-  var base = edges[0]
-  var leftLeg = edges[1]
-  var rightLeg = edges[2]
+function tringle_tracking() {
+  var sides = [] //create an array
+  edges.sides(document.getElementById("base").value);
+  edges.sides(document.getElementById("leftLeg").value);
+  edges.sides(document.getElementById("rightLeg").value);
+  //push edges into array
+  var base = sides[0]
+  var leftLeg = sides[1]
+  var rightLeg = sides[2]
 
   if (base === leftLeg && leftLeg === rightLeg) {
-    output.innerHTML = ("Equilateral Triangele!!")
-  } else if (base === leftLeg || base === rightLeg || leftLeg === rightLeg) {
-    output.innerHTML = ("Isosceles Triangle!!")
+    document.getElementById("output").innerHTML = ("Equilateral Triangele!!")
+  } else if (base === leftLeg && base !== rightLeg || base === rightLeg && base !== leftLeg || leftLeg === rightLeg && leftLeg !== base) {
+    document.getElementById("output").innerHTML = ("Isosceles Triangle!!")
   } else if ((base + leftLeg) <= rightLeg || (base + rightLeg) <= leftLeg || (leftLeg + rightLeg) <= base) {
-    output.innerHTML = ("Not a Triangle!!")
-  } else if (base != leftLeg && base != rightLeg) {
-    output.innerHTML = ("scalene Triange!!")
-  } else if (isNan(base) || isNan(leftLeg) || isNan(rightLeg)) {
-    output.innerHTML = ("not a number")
+    document.getElementById("output").innerHTML = ("Not a Triangle!!")
+  } else if (base !== leftLeg && base !== rightLeg) {
+    document.getElementById("output").innerHTML = ("scalene Triange!!")
+  } else {
+    document.getElementById("output").innerHTML = ("nothing")
   }
-}
-
-function reset() {
-  location.reload();
-}
