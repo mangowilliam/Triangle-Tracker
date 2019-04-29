1fb937d65a578bbe912de8d41bd4ec9bfcf292d2
mangowilliam <williammango2015@gmail.com>
2019-04-29 13:12:34 +0300

    update html with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
-var triangle_tracking = function(base, leftleg, rightleg) {
-    var base = parseInt(document.getElementById("b").value);
-    var leftleg = parseInt(document.getElementById("l").value);
-    var rightleg = parseInt(document.getElementById("r").value);
+function triangle_tracking() {
 
-    var triangles = [];
-    triangles.push(base);
-    triangles.push(leftleg);
-    triangles.push(rightleg);
+  var base = parseInt(document.getElementById("b").value);
+  var leftleg = parseInt(document.getElementById("l").value);
+  var rightleg = parseInt(document.getElementById("r").value);
 
-    if (base === leftleg && leftleg === rightleg) {
-      document.getElementById("output").innerHTML = ("Equilateral Triangele!!")
-    } else if (base === leftleg && base !== rightleg || base === rightleg && base !== leftleg || leftleg === rightleg && leftleg !== base) {
-      document.getElementById("output").innerHTML = ("Isosceles Triangle!!")
-    } else if ((base + leftleg) <= rightleg || (base + rightleg) <= leftleg || (leftleg + rightleg) <= base) {
-      document.getElementById("output").innerHTML = ("Not a Triangle!!")
-    } else if (base !== leftleg && base !== rightleg) {
-      document.getElementById("output").innerHTML = ("scalene Triange!!")
-    } else {
-      document.getElementById("output").innerHTML = ("nothing")
-    }
+  var triangles = [];
+  triangles.push(base);
+  triangles.push(leftleg);
+  triangles.push(rightleg);
+
+  if (base === leftleg && leftleg === rightleg) {
+    document.getElementById("output").innerHTML("Equilateral Triangele!!")
+  } else if (base === leftleg && base !== rightleg || base === rightleg && base !== leftleg || leftleg === rightleg && leftleg !== base) {
+    document.getElementById("output").innerHTML = ("Isosceles Triangle!!")
+  } else if ((base + leftleg) <= rightleg || (base + rightleg) <= leftleg || (leftleg + rightleg) <= base) {
+    document.getElementById("output").innerHTML = ("Not a Triangle!!")
+  } else if (base !== leftleg && base !== rightleg) {
+    document.getElementById("output").innerHTML = ("scalene Triange!!")
+  } else {
+    document.getElementById("output").innerHTML = ("nothing")
+  }
+}
